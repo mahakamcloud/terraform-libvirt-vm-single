6eb33b9ec770ffd2b8847c1e0aa16eaddd827236
Giri Kuncoro <girikuncoro@gmail.com>
2018-12-25 19:02:51 +0700

    Init terraform libvirt module for creating vm with single network interface

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "libvirt_volume" "main" {
+  name   = "${var.instance_name}"
+  pool   = "${var.pool_name}"
+  source = "${var.source_path}"
+  format = "${var.disk_format}"
+  size   = "${var.disk_size}"
+}
+
+resource "libvirt_volume" "secondary" {
+  name   = "${var.instance_name}-secondary"
+  size   = "${var.disk_two_size_gb * 1024 * 1024 * 1024 }"
+}
+
+resource "libvirt_cloudinit" "vm_init" {
+  name  = "${var.instance_name}-init.iso"
+
+  user_data = "${var.user_data}"
+}
+
+resource "libvirt_domain" "vm_domain" {
+  name      = "${var.instance_name}"
+  memory    = "${var.memory_size}"
+  vcpu      = "${var.num_cpu}"
+  autostart = "${var.autostart}"
+
+  cloudinit = "${libvirt_cloudinit.vm_init.id}"
+
+  network_interface {
+    bridge         = "${var.private_network_bridge}"
+    hostname       = "${var.instance_name}"
+    mac            = "${var.mac_address}"
+    wait_for_lease = false
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.main.id}"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.secondary.id}"
+  }
+}
---
+++ var.tf
@@ -0,0 +1,92 @@
+variable libvirt_host {
+  type = "string"
+  description = "IP address of host running libvirt"
+}
+
+variable instance_name {
+  type = "string"
+  description = "name of VM instance"
+}
+
+variable pool_name {
+  type = "string"
+  default = "default"
+  description = "name of pool to store disk and iso image"
+}
+
+variable source_path {
+  type = "string"
+  description = "path to qcow2 base image, can be remote url or local disk path"
+}
+
+variable disk_format {
+  type = "string"
+  default = "qcow2"
+}
+
+variable disk_size {
+  description = "The disk size of primary disk in bytes"
+  type = "string"
+  default = "4096"
+}
+
+variable default_password {
+  type = "string"
+  default = "passw0rd"
+  description = "default password to login to VM when running, it's recommended to disable this manually"
+}
+
+variable memory_size {
+  type = "string"
+  default = "2048"
+  description = "memory size of VM"
+}
+
+variable num_cpu {
+  default = 2
+  description = "number of vCPU which VM has"
+}
+
+variable num_network_interface {
+  default = 1
+  description = "number of network interfaces which VM has"
+}
+
+variable private_network_bridge {
+  type = "string"
+  default = "virbr0"
+  description = "existing network bridge on host that VM needs to connect to private network"
+}
+
+variable user_data {
+  type = "string"
+  description = "cloud init user-data"
+}
+
+variable autostart {
+  default = "true"
+  type = "string"
+  description = "mark libvirt domain to autostart"
+}
+
+variable mac_address {
+  type = "string"
+  description = "mac address of the VM"
+}
+
+variable ip_address {
+  type = "string"
+  description = "ip address of the VM"
+}
+
+variable dns_dhcp_server_username {
+  default = "ubuntu"
+  type = "string"
+  description = "dhcp and dns server ssh username"
+}
+
+variable disk_two_size_gb {
+  default = "1"
+  type = "string"
+  description = "Size of secondary disk"
+}
